aa7ba1c89ac5663f43db5c272b0dd765e29b631d
takuma nishioka <takuma19781192@gmail.com>
2021-06-16 15:34:57 +0900

    first-commit

---
+++ src/firebase/auth.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDi_ubba8ke4BoGUP3A7PDTHI5op3jwjO0",
+  authDomain: "crypto-app-e2585.firebaseapp.com",
+  projectId: "crypto-app-e2585",
+  storageBucket: "crypto-app-e2585.appspot.com",
+  messagingSenderId: "333567728929",
+  appId: "1:333567728929:web:b1489291cd385d20057260"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
